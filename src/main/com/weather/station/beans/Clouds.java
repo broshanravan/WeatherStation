c77c2846bae48810c88bba3836f185db2672d443
Bruce <broshanravan@gmail.com>
2021-04-08 13:35:31 +0100

    completing atributes of new beans

--- src/main/com/weather/station/beans/Clouds.java
+++ src/main/com/weather/station/beans/Clouds.java
@@ -1,4 +1,21 @@
 package main.com.weather.station.beans;
 
 public class Clouds {
+    private int all;
+
+    /**
+     *
+     * @return
+     */
+    public int getAll() {
+        return all;
+    }
+
+    /**
+     *
+     * @param all
+     */
+    public void setAll(int all) {
+        this.all = all;
+    }
 }
--- src/main/com/weather/station/beans/Coordinate.java
+++ src/main/com/weather/station/beans/Coordinate.java
@@ -1,4 +1,38 @@
 package main.com.weather.station.beans;
 
 public class Coordinate {
+    private  double longitude ;
+    private double latitude;
+
+    /**
+     *
+     * @return
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     *
+     * @param longitude
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     *
+     * @param latitude
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 }
